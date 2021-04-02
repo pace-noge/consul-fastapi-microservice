f044b8b78ef3bdd530c71ebb1467ec6d85ce6635
Nasaruddin <nasaruddin@advpubtech.com>
2021-04-02 21:27:58 +0700

    update config and utils

--- microservices/school-service/main.py
+++ microservices/school-service/main.py
@@ -2,13 +2,50 @@ from fastapi import FastAPI
 import httpx
 
 
+from utils import register_to_consul
+
+
+fake_dbs = [
+    {"name": "SD 1", "id": 1},
+    {"name": "SD 2", "id": 2}
+]
+
+
 app = FastAPI()
 
-@app.get("/{name}")
-async def get_school_detail(name: str):
+
+@app.on_event("startup")
+def startup():
+    register_to_consul()
+
+
+@app.get("/")
+async def get_school_list():
+    return fake_dbs
+
+
+
+
+@app.get("/detail/{school_id}")
+async def get_school_detail(school_id: int):
+    school = filter(lambda x: x.get("id") == school_id, fake_dbs)
     students = {}
-    async with httpx.AsyncClient() as client:
-        students = await client.get(f"http://student-service/students/{name}/")
-        students = students.json()
+    students = await client.get(f"http://student_service/school/{scool_id}/")
+    students = students.json()
+
+    ret = {
+        "school": school,
+        "students": students
+    }
     return students
+
+
+@app.get("/health")
+def health_status():
+    return {"status": "healthy"}
+
+
+@app.get("/register-to-consul")
+async def register_to_consul_manually():
+    return register_to_consul()
 
--- microservices/student-service/main.py
+++ microservices/student-service/main.py
@@ -5,9 +5,9 @@ import httpx
 from time import sleep
 import logging
 
-logger = logging.getLogger(__name__)
 
 from config import Config
+from utils import register_to_consul
 
 configuration = Config()
 
@@ -15,43 +15,29 @@ configuration = Config()
 # app = FastAPI()
 
 
+fake_dbs = [
+    {'name': 'Bilal', 'class': 'V', 'school_id': 1},
+    {'name': 'Ganta', 'class': 'IX', 'school_id': 1},
+    {'name': 'Azril', 'class': 'IV', 'school_id': 2}
+]
+
+
 def create_app():
     app = FastAPI(title="student-service")
-    app.logger = logger
     return app
 
 
-def register_to_consul():
-    url = "http://consul:8500/v1/agent/service/register"
-    data = {
-        "Name": "StudentService",
-        "Tags": ['student'],
-        'Address': configuration.address,
-        'Port': configuration.port,
-        "Check": {
-            'http': f'http://{configuration.address}:{configuration.port}/health',
-            'interval': '10s'
-        }
-    }
-    print('Service registration parameters: ', data)
-    res = {"message": "registering"}
-    res = httpx.put(url, data=json.dumps(data))
-    return res.text
-
-
-
 app = create_app()
+
 
 @app.on_event("startup")
 def startup_event():
     register_to_consul()
 
+
 @app.get("/")
 async def list_students():
-    return [
-        {"name": "Bilal Ibnu", "class_name": "Class V"},
-        {"name": "Ganta", "class_name": "Class IX"}
-    ]
+    return fake_dbs
 
 
 @app.get("/health")
@@ -59,19 +45,18 @@ async def health_status():
     return {"status": "healthy"}
 
 
-@app.get("/ping-consul")
-async def ping_consul():
-    with httpx.AsyncClient() as client:
-        r = client.get("http://consul:8500/agent/services")
-    return r.json()
-
 
 @app.get("/register-service")
 async def register():
     return register_to_consul()
 
-@app.get("/{name}")
+
+@app.get("/name/{name}")
 async def get_student(name: str):
-    return {"name": name}
+    return filter(lambda x: x.get('name') == name, fake_dbs)
+
+@app.get("/school/{school_id}")
+async def filter_student_by_school(school_id: int):
+    return filter(lambda x: x.get("school_id") == school_id, fake_dbs)
 
 
